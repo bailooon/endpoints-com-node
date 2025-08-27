1370cfbcc102c6c2209b80da4ad0b00eb3a459eb
bailooon <matheus@bailon.com.br>
2025-08-27 20:48:33 -0300

    configurando o router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,135 +1,153 @@
 const express = require("express");
+const product = require("../models/product");
 const router = new express.Router();
 
-let users = [
-  { id: 1, nome: "Matheus", idade: 20 },
-  { id: 2, nome: "Kaique", idade: 21 },
-];
-
-//endpoint
-//get
-router.get("/", (req, res, next) => {
-  try {
-    res.status(200).send({
-      success: true,
-      message: "Usuários encontrados",
-      data: users,
-      total: users.length,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Erro interno",
-      error: error.message,
-    });
-  }
-});
-
-//post
-router.post("/", (req, res, next) => {
-
-    try{
-        const {nome, idade} = req.body
-
-        if(!nome || !idade){
-            return res.status(400).json({
-                success:false,
-                message: "Favor, enviar os campos: nome e idade"
-            })
-        }
-
-        const newId = users.length + 1;
-        const newUser = {
-            id: newId,
-            nome,
-            idade
-        }
-        users.push(newUser);
-
-        console.log(newId);
-        //Status code 201: created
-        res.status(201).json({
-            success:true,
-            message: "Usuario criado com sucesso",
-            data:newId
-        })
-    }catch(error){
-        res.status(500).json({
-            success:false,
-            message:"Ocorreu um erro na criação do usuario"
-        })
-
+router.get("/", (req, res) => {
+  res.json({
+    message: "Api no ar!",
+    endpoints: {
+      products: "/products",
+      health: "/health",
     }
-
-});
-
-//put
-router.put("/:id", (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const {nome, idade} = req.body
-
-        if(!nome || !idade){
-            return res.status(400).json({
-                success:false,
-                message: "Favor, enviar os campos: nome e idade"
-            })
-        }
-
-        const 
-        userFind = users.findIndex((u) => u.id == id)
-
-        if(userFind === -1){
-            return res.status(404).json({
-                success:false,
-                message: "usuário não encontrado"
-            })
-        }
-
-        users[userFind] = {
-            id,
-            nome,
-            idade
-        }
-
-        res.status(200).json({
-            success:true,
-            message: "Usuário atualizado com sucesso!"
-        });
+  })
 })
 
-//delete
-router.delete("/:id", (req, res, next) => {
-  try{
-    const id = parseInt(req.params.id)
-    const userIndex = users.findIndex(u => u.id === id)
-    console.log(`index encontrado: ${userIndex}`)
-    if(userIndex === -1){
-      return res.status(404).json(
-        {
-          success: false, 
-          message: "Identificador não encontrado"
-        }
-      )
-    }
-    //remover da lista
-    const deleteUser = users[userIndex] 
-    users.splice(userIndex, 1) // removendo 
+router.get('/health', (req, res) => {
+  res.json({
+    status: 'Ok',
+    timestamp: new Date().toISOString()
+  })
+})
 
-    res.status(200).json({
-        success: true,
-        message: 'Removido com sucesso'
-    })
+// let users = [
+//   { id: 1, nome: "Matheus", idade: 20 },
+//   { id: 2, nome: "Kaique", idade: 21 },
+// ];
+
+// //endpoint
+// //get
+// router.get("/", (req, res, next) => {
+//   try {
+//     res.status(200).send({
+//       success: true,
+//       message: "Usuários encontrados",
+//       data: users,
+//       total: users.length,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "Erro interno",
+//       error: error.message,
+//     });
+//   }
+// });
+
+// //post
+// router.post("/", (req, res, next) => {
+
+//     try{
+//         const {nome, idade} = req.body
+
+//         if(!nome || !idade){
+//             return res.status(400).json({
+//                 success:false,
+//                 message: "Favor, enviar os campos: nome e idade"
+//             })
+//         }
+
+//         const newId = users.length + 1;
+//         const newUser = {
+//             id: newId,
+//             nome,
+//             idade
+//         }
+//         users.push(newUser);
+
+//         console.log(newId);
+//         //Status code 201: created
+//         res.status(201).json({
+//             success:true,
+//             message: "Usuario criado com sucesso",
+//             data:newId
+//         })
+//     }catch(error){
+//         res.status(500).json({
+//             success:false,
+//             message:"Ocorreu um erro na criação do usuario"
+//         })
+
+//     }
+
+// });
+
+// //put
+// router.put("/:id", (req, res, next) => {
+//     const id = parseInt(req.params.id);
+//     const {nome, idade} = req.body
+
+//         if(!nome || !idade){
+//             return res.status(400).json({
+//                 success:false,
+//                 message: "Favor, enviar os campos: nome e idade"
+//             })
+//         }
+
+//         const 
+//         userFind = users.findIndex((u) => u.id == id)
+
+//         if(userFind === -1){
+//             return res.status(404).json({
+//                 success:false,
+//                 message: "usuário não encontrado"
+//             })
+//         }
+
+//         users[userFind] = {
+//             id,
+//             nome,
+//             idade
+//         }
+
+//         res.status(200).json({
+//             success:true,
+//             message: "Usuário atualizado com sucesso!"
+//         });
+// })
+
+// //delete
+// router.delete("/:id", (req, res, next) => {
+//   try{
+//     const id = parseInt(req.params.id)
+//     const userIndex = users.findIndex(u => u.id === id)
+//     console.log(`index encontrado: ${userIndex}`)
+//     if(userIndex === -1){
+//       return res.status(404).json(
+//         {
+//           success: false, 
+//           message: "Identificador não encontrado"
+//         }
+//       )
+//     }
+//     //remover da lista
+//     const deleteUser = users[userIndex] 
+//     users.splice(userIndex, 1) // removendo 
+
+//     res.status(200).json({
+//         success: true,
+//         message: 'Removido com sucesso'
+//     })
     
-  }catch(error){
-    console.info(erro)
-    res.status(500).json(
-      {
-        success:false, 
-        message: "Erro interno ao deletar o usuário"
-      }
-    )
-  }
+//   }catch(error){
+//     console.info(erro)
+//     res.status(500).json(
+//       {
+//         success:false, 
+//         message: "Erro interno ao deletar o usuário"
+//       }
+//     )
+//   }
 
-});
+// });
 
 module.exports = router;
---
+++ src/routes/product-route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = new express.Router();
+const ProductController = require('../controllers/product-controller');
+
+router.get('/', ProductController.getAllProducts);
+router.get('/:id', ProductController.getProductById);
+router.post('/', ProductController.createProduct);
+router.put('/:id', ProductController.updateProduct);
+router.delete('/:id', ProductController.deleteProduct);
+module.exports = router;
